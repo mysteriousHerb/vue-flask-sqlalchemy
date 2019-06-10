2654a81bd7a46474e2c83544bd70a87be1e9cd96
herbzhao <herbzhao@gmail.com>
2019-06-10 22:15:24 +0200

    with build

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -11,7 +10,6 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      // component: Home
       component: () => import(/* webpackChunkName: "about" */ './views/Home.vue')
     },
     {
