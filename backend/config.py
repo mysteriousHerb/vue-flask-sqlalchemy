836a5056dbbff3c2fec288a0fdb54b168c05a83b
herbzhao <herbzhao@gmail.com>
2019-06-12 09:07:24 +0200

    debug for deployment, removing debug=true

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,5 @@ import os
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-key"
-    DEBUG = True
     CSRF_ENABLED = True
     UPLOAD_FOLDER = r'saved_files'
--- backend/test_app.py
+++ backend/test_app.py
@@ -6,4 +6,7 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    # If you have the debugger disabled or trust the users on your network, 
+    # you can make the server publicly available simply by
+    #  adding --host=0.0.0.0 to the command line
+    app.run(host='0.0.0.0', debug=False)
